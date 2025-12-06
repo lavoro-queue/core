d669944d7a9e65f7a7707ab88fb193271950e74c
Aleksei Ivanov <contact@aleksei.dev>
2025-12-06 17:26:00 +0000

    chore(tests): comment out overlapping job scheduling test for future implementation

--- tests/functional/postgres_job_schedule.spec.ts
+++ tests/functional/postgres_job_schedule.spec.ts
@@ -128,5 +128,97 @@ describe(
       expect(stats.totalCount).toBeGreaterThanOrEqual(minExpectedJobRuns)
       expect(stats.totalCount).toBeLessThanOrEqual(maxExpectedJobRuns)
     })
+
+    // test('should overlap when using different lock providers', async () => {
+    //   jobRuns = 0
+
+    //   const pending1 = Schedule.job(LongRunningJob, {})
+    //     .every('second')
+    //     .onConnection('main')
+    //     .onQueue('high-throughput')
+    //     .lockFor(LongRunningJob.duration)
+    //   // ;(pending1 as any).lockServiceResolver = () =>
+    //   //   new LockFactory(
+    //   //     knexStore({
+    //   //       connection: knex({
+    //   //         client: 'pg',
+    //   //         connection: {
+    //   //           host: ctx.getPostgresConfig().host,
+    //   //           port: Number(ctx.getPostgresConfig().port),
+    //   //           user: ctx.getPostgresConfig().user,
+    //   //           password: ctx.getPostgresConfig().password,
+    //   //           database: ctx.getPostgresConfig().database,
+    //   //         },
+    //   //       }),
+    //   //       autoCreateTable: true,
+    //   //       tableName: 'lavoro_locks_1',
+    //   //     }).factory(),
+    //   //   )
+
+    //   // ;(pending1 as any).lockServiceResolver = () =>
+    //   //   new LockFactory(memoryStore().factory())
+
+    //   const pending2 = Schedule.job(LongRunningJob, {})
+    //     .every('second')
+    //     .onConnection('main')
+    //     .onQueue('high-throughput')
+    //     .lockFor(LongRunningJob.duration)
+    //   // ;(pending2 as any).lockServiceResolver = () =>
+    //   //   new LockFactory(
+    //   //     knexStore({
+    //   //       connection: knex({
+    //   //         client: 'pg',
+    //   //         connection: {
+    //   //           host: ctx.getPostgresConfig().host,
+    //   //           port: Number(ctx.getPostgresConfig().port),
+    //   //           user: ctx.getPostgresConfig().user,
+    //   //           password: ctx.getPostgresConfig().password,
+    //   //           database: ctx.getPostgresConfig().database,
+    //   //         },
+    //   //       }),
+    //   //       autoCreateTable: true,
+    //   //       tableName: 'lavoro_locks_2',
+    //   //     }).factory(),
+    //   //   )
+
+    //   // Make cron job names unique to avoid conflicts.
+    //   // We can do it like this since queue name is used for cron job name.
+    //   ;(pending1 as any)._queue += '_1'
+    //   ;(pending2 as any)._queue += '_2'
+
+    //   // ;(pending2 as any).lockServiceResolver = () =>
+    //   //   new LockFactory(memoryStore().factory())
+
+    //   // await pending1
+
+    //   await Promise.all([pending1, pending2])
+
+    //   const minExpectedJobRuns = 6
+    //   const maxExpectedJobRuns = 10
+
+    //   await new Promise((resolve) =>
+    //     setTimeout(resolve, PG_BOSS_POLLING + LongRunningJob.duration),
+    //   )
+
+    //   Schedule.clear()
+
+    //   await new Promise((resolve) =>
+    //     setTimeout(resolve, PG_BOSS_POLLING + LongRunningJob.duration),
+    //   )
+
+    //   // expect(jobRuns).toBeGreaterThanOrEqual(minExpectedJobRuns)
+    //   // expect(jobRuns).toBeLessThanOrEqual(maxExpectedJobRuns)
+
+    //   const stats = await ctx.getPgBossStats(
+    //     'main',
+    //     'high-throughput',
+    //     LongRunningJob,
+    //   )
+
+    //   console.log({ jobRuns, stats: stats.totalCount })
+
+    //   // expect(stats.totalCou  nt).toBeGreaterThanOrEqual(minExpectedJobRuns)
+    //   // expect(stats.totalCount).toBeLessThanOrEqual(maxExpectedJobRuns)
+    // })
   },
 )
